f68ea1588ef0211367f60762a56b6465ee5957bb
Guilleber <leberre_guillaume@orange.fr>
2016-10-28 15:53:26 +0200

    commit final

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -10,7 +10,7 @@ class Question(models.Model):
     def __str__(self):
         return self.question_text
     def was_published_recently(self):
-	now = timezone.now()
+        now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
 
